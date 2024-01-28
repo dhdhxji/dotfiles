382a3044348e4a37909406eb1bb409120c9d44cc
Alex <oleksandrkovalchuk1998@gmail.com>
2024-01-28 16:24:43 +0200

    Add neovim config

---
+++ .config/nvim/lua/custom/init.lua
@@ -0,0 +1,15 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.listchars = 'eol:↩'
+vim.opt.list = true
