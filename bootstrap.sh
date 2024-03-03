2c66868ab7fd7fc9a373e095976e771efa14520f
Alex <oleksandrkovalchuk1998@gmail.com>
2024-03-03 22:32:38 +0200

    Minor bootstrap fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-git clone https://github.com/dhdhxji/dotfiles.git /tmp/dotfiles
+git clone https://github.com/dhdhxji/dotfiles.git /tmp/dotfiles || \
+    echo "WARN: Dotfiles repo already exist. Last commit is $(git log  HEAD^..HEAD --pretty=format:'%h: %s' --no-patch)"
 cd /tmp/dotfiles
 
 
@@ -12,8 +13,7 @@ cp .tmux.conf ~
 
 BASHRC_CUSTOM_FILENAME=".bashrc.custom"
 cp $BASHRC_CUSTOM_FILENAME ~/$BASHRC_CUSTOM_FILENAME
-[ ! -f ~/.bashrc ] && touch ~/.bashrc
-[ -z $(grep "source ~/$BASHRC_CUSTOM_FILENAME" ~/.bashrc) ] && echo "source ~/$BASHRC_CUSTOM_FILENAME" >> ~/.bashrc
+! grep -q "source ~/$BASHRC_CUSTOM_FILENAME" ~/.bashrc && echo "source ~/$BASHRC_CUSTOM_FILENAME" >> ~/.bashrc
 
 mkdir -p ~/.config/
 cp -r .config/nvim ~/.config/nvim
@@ -21,3 +21,4 @@ cp -r .config/nvim ~/.config/nvim
 [ ! -f ~/.gitconfig ] && cp .gitconfig ~ || echo "WARN: .gitconfig already exists, skipping (may require manual configuration)"
 
 cd -
+rm -rf /tmp/dotfiles
