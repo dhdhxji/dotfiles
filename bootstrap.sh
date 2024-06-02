7553fbeeaa68f8c687b14be48743041fa8d7c386
dhdhxji <oleksandrkovalchuk1998@gmail.com>
2024-06-02 20:21:44 +0300

    Squashed commit of the following:
    commit 6e179335712c72da2a7dba44b993952e810354ae
    Author: dhdhxji <oleksandrkovalchuk1998@gmail.com>
    Date:   Sun Jun 2 20:20:44 2024 +0300
    
        Fix automatic yadm bootstrap
    
    commit 5fcbb1b623349588e96cbaace3b24a1731024323
    Author: dhdhxji <oleksandrkovalchuk1998@gmail.com>
    Date:   Sun Jun 2 19:46:25 2024 +0300
    
        Do not clone TPM if already exist
    
    commit b130fb9336612025c37fb552cf50944eed21fc21
    Author: dhdhxji <oleksandrkovalchuk1998@gmail.com>
    Date:   Sun Jun 2 18:03:15 2024 +0300
    
        Update bootstrap scripts
    
    commit 77828a87e3edcd70312261f80ae1d6ed5a60527f
    Author: dhdhxji <oleksandrkovalchuk1998@gmail.com>
    Date:   Sun Jun 2 17:26:29 2024 +0300
    
        Migrate to yadm

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-set -e
+YADM_LOCATION="$HOME/.local/bin/yadm"
+mkdir -p $(dirname $YADM_LOCATION)
+curl -fLo $YADM_LOCATION https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x $YADM_LOCATION
 
-git clone https://github.com/dhdhxji/dotfiles.git /tmp/dotfiles || \
-    echo "WARN: Dotfiles repo already exist. Last commit is $(git log  HEAD^..HEAD --pretty=format:'%h: %s' --no-patch)"
-cd /tmp/dotfiles
-
-
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || true
-cp .tmux.conf ~
-
-BASHRC_CUSTOM_FILENAME=".bashrc.custom"
-cp $BASHRC_CUSTOM_FILENAME ~/$BASHRC_CUSTOM_FILENAME
-! grep -q "source ~/$BASHRC_CUSTOM_FILENAME" ~/.bashrc && echo "source ~/$BASHRC_CUSTOM_FILENAME" >> ~/.bashrc
-
-mkdir -p ~/.config/
-cp -r .config/nvim ~/.config/nvim
-
-[ ! -f ~/.gitconfig ] && cp .gitconfig ~ || echo "WARN: .gitconfig already exists, skipping (may require manual configuration)"
-
-cd -
-rm -rf /tmp/dotfiles
+$YADM_LOCATION clone https://github.com/dhdhxji/dotfiles --bootstrap
