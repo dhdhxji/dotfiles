0045a574e9646d06da72e09e2983eeeb391d2e37
dhdhxji <oleksandrkovalchuk1998@gmail.com>
2024-06-09 00:12:38 +0300

    Update nvim tab settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,9 @@
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
+vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.listchars = 'eol:↩'
 vim.opt.list = true
+vim.opt.mouse = 'a'
