790bf56a0d1642fb5b1a2a2dc66b49319f1d3a09
dhdhxji <oleksandrkovalchuk1998@gmail.com>
2024-06-28 20:08:08 +0300

    Install tpm at first tmux load

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,4 @@ STOW_MODULES="git nvim tmux bash"
 stow ${STOW_MODULES} --target=$HOME
 
 # And run additional setups here 
-if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    mkdir -p $HOME/.tmux/plugins
-    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm || true
-fi
-
 ! grep -q "source ~/.bashrc.custom" ~/.bashrc && echo "source ~/.bashrc.custom" >> ~/.bashrc
